a5bbaac27f7a36e69dd83c710a98a9af07c06b5e
Emma Barlow <e.barlow159@gmail.com>
2023-07-20 21:11:03 +0100

    added UI

--- app.js
+++ app.js
@@ -1,59 +1,53 @@
-//array containing the choices the computer can make
-const choices = ["rock", "paper", "scissors"];
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".playerBtn");
+let player;
+let computer;
+let result;
 
-function getComputerChoice(arr) {
-  //return random item from the array the function is given
-  return arr[Math.floor(Math.random() * arr.length)];
+choiceBtns.forEach(btn => btn.addEventListener("click", () => {
+  player = btn.textContent;
+  computerChoice();
+  playerText.textContent = `Player: ${player}`;
+  computerText.textContent = `Computer: ${computer}`;
+  resultText.textContent = playRound()
+}))
+
+function computerChoice() {
+  const randomNumber = Math.floor(Math.random()*3)+1
+  switch(randomNumber){
+    case 1:
+      computer = "rock";
+      break;
+    case 2:
+      computer = "paper";
+      break;
+    case 3:
+      computer = "scissors";
+      break;
+  };
 }
 
-//getting computers choice
-let computerChoice = getComputerChoice(choices);
-console.log(computerChoice);
-
-function getPlayerChoice() {
-  //Prompt for player to enter choice
-  let choice = prompt("Please enter rock, paper or scissors");
-  //Return choice in lower case
-  return choice.toLowerCase();
-}
-
-//Getting players choice
-let playerChoice = getPlayerChoice();
-console.log(playerChoice);
-
-function playRound(computerChoice, playerChoice) {
+function playRound() {
   //If choice is the same
-  if (computerChoice === playerChoice) {
+  if (computer === player) {
     return "It's a tie!";
   } else if (
     //Choices where the player would win
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "scissors" && computerChoice === "paper") ||
-    (playerChoice === "paper" && computerChoice === "rock")
+    (player === "rock" && computer === "scissors") ||
+    (player === "scissors" && computer === "paper") ||
+    (player === "paper" && computer === "rock")
   ) {
     return "Player wins";
   } else if (
     //choices where the computer would win
-    (computerChoice === "rock" && playerChoice === "scissors") ||
-    (computerChoice === "scissors" && playerChoice === "paper") ||
-    (computerChoice === "paper" && playerChoice === "rock")
+    (computer === "rock" && player === "scissors") ||
+    (computer === "scissors" && player === "paper") ||
+    (computer === "paper" && player === "rock")
   ) {
     return "Computer Wins";
   } else {
     return "Invalid choice!";
   }
 }
-
-//Runs the playRound function and returns the winner
-let winner = playRound(computerChoice, playerChoice);
-console.log(winner);
-
-/*
-function game(computerChoice, playerChoice) {
-    let i = 0
-    while (i < 5) {
-        code
-        i++
-    }
-}
-*/
